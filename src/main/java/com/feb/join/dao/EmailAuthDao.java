38e65f637e95b5b905c8d695e3f1d793c55b870a
J0a0J <jiweon.j.kim@gmail.com>
2024-02-24 23:22:31 +0900

    Ajax 실습
    회원가입 후 인증메일 전송 시도.
    그런데 DB에 메일 관련 기록은 저장되지만 메일은 전송되지 않음.

---
+++ src/main/java/com/feb/join/dao/EmailAuthDao.java
@@ -0,0 +1,12 @@
+package com.feb.join.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.feb.join.entity.EmailAuth;
+
+@Repository
+public interface EmailAuthDao {
+	
+	public int addEmailAuth(EmailAuth dto);
+
+}
